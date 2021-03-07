2d41a755ed075dca94dd45a520f8698181efcc00
Frederik Jensen <frederikhj0@gmail.com>
2021-03-07 15:29:18 +0100

    Update Program.cs

--- DBDCA1/DBDCA1/Program.cs
+++ DBDCA1/DBDCA1/Program.cs
@@ -1,16 +1,41 @@
 ﻿using Infrastructure;
 using Microsoft.Data.SqlClient;
 using System;
+using System.Collections.Generic;
 
 namespace DBDCA1
 {
     public class Program
     {
+        static List<string> options = new List<string>()
+        {
+            "Create Department"
+        };
         static void Main(string[] args)
         {
             var ctx = new CompanyContext();
             var service = new DepartmentService(ctx);
 
+            Console.WriteLine("Please type index for selected query");
+
+            for (int i = 0; i < options.Count; i++)
+            {
+                Console.WriteLine($"{i+1}. {options[i]}");
+            }
+
+            var isValid = int.TryParse(Console.ReadLine(), out int choice);
+
+            if (isValid)
+            {
+                Options(choice, service);
+            } else
+            {
+                Console.WriteLine("Not valid");
+            }
+        }
+
+        static void CreateDepartment(DepartmentService service)
+        {
             Console.WriteLine("Enter name of department");
             var dName = Console.ReadLine();
             Console.WriteLine("Enter SSN of manager");
@@ -23,13 +48,27 @@ namespace DBDCA1
                 {
                     service.CreateDepartment(dName, mgrSSN);
                     Console.WriteLine("Finished writing");
-                } catch (SqlException ex)
+                }
+                catch (SqlException ex)
                 {
                     Console.WriteLine(ex.Message);
                 }
             }
             Console.WriteLine("Press enter to quit");
             Console.ReadLine();
+        }
+
+        static void Options(int option, DepartmentService service)
+        {
+            switch(option)
+            {
+                case 1:
+                    CreateDepartment(service);
+                    break;
+                default:
+                    Console.WriteLine("Choise does not exists");
+                    break;
+            }
         }
     }
 }
