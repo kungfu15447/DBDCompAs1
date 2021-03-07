bebc267bb1ef5fec4653677e2c17091ecc89643e
Frederik Jensen <frederikhj0@gmail.com>
2021-03-07 12:30:00 +0100

    Made project

---
+++ DBDCA1/DBDCA1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DBDCA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
