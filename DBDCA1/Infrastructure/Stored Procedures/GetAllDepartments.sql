42586e6ba4382a0bec99ba6870c1cd8e53153179
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2021-03-07 15:25:02 +0100

    GetAllDepartments is made

---
+++ DBDCA1/Infrastructure/Stored Procedures/GetAllDepartments.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE usp_GetAllDepartments
+AS 
+BEGIN
+
+SELECT d.DNumber, d.DName, d.MgrSSN, d.MgrStartDate, COUNT(e.SSN) AS EmpCount
+    FROM Department AS d 
+        LEFT JOIN Employee AS e ON e.DNo = d.DNumber;
+
+END
