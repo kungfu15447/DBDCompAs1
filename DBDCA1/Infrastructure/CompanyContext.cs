e0798e0c3c721e1bf31f4e82c7694765354043db
kristianUrup <43743914+kristianUrup@users.noreply.github.com>
2021-03-07 16:08:12 +0100

    commit

---
+++ DBDCA1/Infrastructure/CompanyContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext()
+        {
+        }
+
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     if (!optionsBuilder.IsConfigured)
+        //     {
+        //         optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-Q64P9QF;Initial Catalog=Company;Integrated Security=True");
+        //     }
+        // }
+
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     modelBuilder.Entity<Department>()
+        //         .HasKey(d => d.DNumber);
+        // }
+
+        // public DbSet<Department> Department { get; set; }
+    }
+}
---
+++ DBDCA1/Infrastructure/DepartmentService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Infrastructure
+{
+    public class DepartmentService
+    {
+        private CompanyContext _ctx;
+
+        public DepartmentService(CompanyContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        // public void CreateDepartment(string dName, decimal mgrSSN)
+        // {
+        //     _ctx.Database.ExecuteSqlRaw("EXECUTE dbo.usp_CreateDepartment {0}, {1}", dName, mgrSSN);
+        // }
+
+        // public virtual List<Department> GetAllDepartments()
+        // {
+        //     return _ctx.Department.FromSqlRaw("EXECUTE dbo.usp_GetAllDepartments").ToList();
+        // }
+    }
+}
