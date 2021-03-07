a8b636e657077f74f5dcd2a1bdd23134d6f3b054
Frederik Jensen <frederikhj0@gmail.com>
2021-03-07 15:17:24 +0100

    Will now be able to execute CreateDepartment stored procedure

--- DBDCA1/DBDCA1/Program.cs
+++ DBDCA1/DBDCA1/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Infrastructure;
+using Microsoft.Data.SqlClient;
+using System;
 
 namespace DBDCA1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var ctx = new CompanyContext();
+            var service = new DepartmentService(ctx);
+
+            Console.WriteLine("Enter name of department");
+            var dName = Console.ReadLine();
+            Console.WriteLine("Enter SSN of manager");
+            var couldParse = decimal.TryParse(Console.ReadLine(), out decimal mgrSSN);
+
+            if (couldParse && !String.IsNullOrEmpty(dName))
+            {
+                Console.WriteLine("Writing to database");
+                try
+                {
+                    service.CreateDepartment(dName, mgrSSN);
+                    Console.WriteLine("Finished writing");
+                } catch (SqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.WriteLine("Press enter to quit");
+            Console.ReadLine();
         }
     }
 }
---
+++ DBDCA1/Infrastructure/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Models
+{
+    public class Department
+    {
+        public int DNumber { get; set; }
+        public string DName { get; set; }
+        public DateTime MgrStartDate { get; set; }
+        public decimal MgrSSN { get; set; }
+    }
+}
